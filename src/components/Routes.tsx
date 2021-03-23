4414a727f2b90525afba721699b34d75f896d834
Matthew Francis <mattfrancis888@gmail.com>
2021-03-23 10:52:12 -0400

    Initial development commit.

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,11 +1,11 @@
 import React, { Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
-
+import Header from "./Header";
 const Routes: React.FC<{}> = () => {
     return (
         <React.Fragment>
-            {/* <Header /> */}
+            <Header />
             <Switch>
                 <Route path="/" exact component={Home} />
             </Switch>
