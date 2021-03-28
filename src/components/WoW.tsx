1c5721e12d3f17dd180c509b7f0c61ce11e2bf52
Matthew Francis <mattfrancis888@gmail.com>
2021-03-28 19:27:59 -0400

    Created 'begin your adventure' and 'welcome back to azeroth' section.

--- src/components/WoW.tsx
+++ src/components/WoW.tsx
@@ -31,7 +31,7 @@ const WoW: React.FC<{}> = () => {
                 )}
 
                 {width < LG_SCREEN_SIZE && (
-                    <div className="mobileBannerImageWrap">
+                    <div className="wowMobileBannerImageWrap">
                         <img
                             src="https://bnetcmsus-a.akamaihd.net/cms/template_resource/UXK7M895FJ3Q1606119959229.jpg"
                             alt="banner"
@@ -46,6 +46,50 @@ const WoW: React.FC<{}> = () => {
                         Castle Nathria, Sire Denathrius' fortress in the dark
                         heart of Revendreth.
                     </p>
+                </div>
+            </div>
+            <div className="wowContentContainer">
+                <div className="wowBeginAndReturningWrap">
+                    <div className="wowBeginContainer">
+                        <div className="wowBeginImageWrap">
+                            <img
+                                src="https://bnetcmsus-a.akamaihd.net/cms/template_resource/bd/BDMOXH9OQ6B31594740408177.png"
+                                alt="adventurers"
+                            ></img>
+                        </div>
+                        <div className="wowBeginAndReturnTextWrap">
+                            <p className="wowBeginAndReturnSubTitle">
+                                New to WoW
+                            </p>
+                            <h1 className="wowBeginAndReturnTitle">
+                                Begin Your Adventure
+                            </h1>
+                            <p className="wowBeginAndReturnDesc">
+                                Start your journey through Azeroth here and try
+                                WoW for free!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="wowReturningContainer">
+                        <div className="wowReturningImageWrap">
+                            <img
+                                src="https://bnetcmsus-a.akamaihd.net/cms/template_resource/et/ETA7LQVBODH61594740917046.png"
+                                alt="ragnarok and lich king"
+                            ></img>
+                        </div>
+                        <div className="wowBeginAndReturnTextWrap">
+                            <p className="wowBeginAndReturnSubTitle">
+                                Returning Players
+                            </p>
+                            <h1 className="wowBeginAndReturnTitle">
+                                Welcome Back to Azeroth
+                            </h1>
+                            <p className="wowBeginAndReturnDesc">
+                                Learn how to get back in the game and play the
+                                latest content for WoW and WoW Classic.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
