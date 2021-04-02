8d8f8211bb9073fb76ec14291a1e10f11bcd1615
Matthew Francis <mattfrancis888@gmail.com>
2021-04-01 20:16:12 -0400

    Added fade animations for background of story slide.

--- src/components/Overwatch2SlideStory.tsx
+++ src/components/Overwatch2SlideStory.tsx
@@ -4,21 +4,32 @@ import _ from "lodash";
 import useWindowDimensions from "../windowDimensions";
 const timer = 1000;
 
+const stories = [
+    {
+        image:
+            "https://overwatch2-static.playoverwatch.com/9bff17453c4b61344f201071908821fc391221ca/static/images/timeline/2560/omnic-crisis.jpg",
+    },
+    {
+        image:
+            "https://overwatch2-static.playoverwatch.com/9bff17453c4b61344f201071908821fc391221ca/static/images/timeline/2560/overwatch-established.jpg",
+    },
+];
+
 const Overwatch2SlideStory: React.FC<{}> = () => {
     const [progress, setProgress] = useState(0);
 
-    // const fill = useTransition(progress, {
-    //     from: {
-    //         width: "0%",
-    //     },
-    //     enter: {
-    //         width: `${progress}%`,
-    //     },
+    const fade = useTransition(progress, {
+        from: {
+            opacity: 0,
+        },
+        enter: {
+            opacity: 1,
+        },
 
-    //     config: {
-    //         duration: timer,
-    //     },
-    // });
+        config: {
+            duration: timer,
+        },
+    });
 
     const fill = useSpring({
         from: {
@@ -33,23 +44,26 @@ const Overwatch2SlideStory: React.FC<{}> = () => {
         },
     });
 
-    // const [startPod, setStartPod] = useState(false);
-
-    // const { podX, podY } = useSpring({
-    //     podX: startPod ? 5 : 0,
-    //     podY: startPod ? 60 : 0,
-
-    //     config: {
-    //         duration: podTimer / 2,
-    //         mass: 10,
-    //         tension: 550,
-    //         friction: 140,
-    //     },
-    // });
-
+    const renderBackground = () => {
+        if (progress === 25) {
+            return stories[0].image;
+        } else if (progress === 100) {
+            return stories[1].image;
+        }
+    };
     return (
         <React.Fragment>
-            <div>
+            <div className="overwatch2StoryContainer">
+                {fade((style, index) => {
+                    return (
+                        <animated.img
+                            style={style}
+                            className="overwatch2StoryBackgroundImage"
+                            src={renderBackground()}
+                            alt="background"
+                        />
+                    );
+                })}
                 <div className="overwatch2StoryTimelineContainer">
                     {/* {fill((style, index) => (
                     <animated.div
@@ -73,10 +87,25 @@ const Overwatch2SlideStory: React.FC<{}> = () => {
                             onClick={() => setProgress(25)}
                             className="overwatch2StoryTimelineProgressControlSectionWrap"
                         >
-                            <div className="overWatch2StoryTimlineProgressTitleAndImageWrap">
-                                {/* {progress === 25 && (
-                                        <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*"></img>
-                                    )} */}
+                            <div className="overWatch2StoryTimelineProgressTitleAndImageWrap">
+                                {progress === 25 &&
+                                    fade((style, index) => {
+                                        return (
+                                            <animated.div
+                                                className="overwatch2StoryTimelineControlImageWrap"
+                                                style={style}
+                                            >
+                                                <img
+                                                    src="https://overwatch2-static.playoverwatch.com/9bff17453c4b61344f201071908821fc391221ca/static/images/timeline/thumbnails/honor-and-glory.jpg"
+                                                    alt=""
+                                                />
+                                                <img
+                                                    src="https://overwatch2-static.playoverwatch.com/9bff17453c4b61344f201071908821fc391221ca/static/images/timeline/thumbnails/omnic-crisis.jpg"
+                                                    alt=""
+                                                />
+                                            </animated.div>
+                                        );
+                                    })}
 
                                 <p className="overWatch2StoryTimlineProgressTitle">
                                     Omnic Crisis
@@ -87,10 +116,21 @@ const Overwatch2SlideStory: React.FC<{}> = () => {
                             onClick={() => setProgress(100)}
                             className="overwatch2StoryTimelineProgressControlSectionWrap"
                         >
-                            <div className="overWatch2StoryTimlineProgressTitleAndImageWrap">
-                                {progress === 100 && (
-                                    <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*"></img>
-                                )}
+                            <div className="overWatch2StoryTimelineProgressTitleAndImageWrap">
+                                {progress === 100 &&
+                                    fade((style, index) => {
+                                        return (
+                                            <animated.div
+                                                className="overwatch2StoryTimelineControlImageWrap"
+                                                style={style}
+                                            >
+                                                <img
+                                                    src=" https://overwatch2-static.playoverwatch.com/9bff17453c4b61344f201071908821fc391221ca/static/images/timeline/thumbnails/overwatch-established.jpg"
+                                                    alt=""
+                                                />
+                                            </animated.div>
+                                        );
+                                    })}
 
                                 <p className="overWatch2StoryTimlineProgressTitle">
                                     Overwatch Established
