458fd92c4fd30091c47ad2de116d082a949991b7
Matthew Francis <mattfrancis888@gmail.com>
2021-04-02 17:08:04 -0400

    Created accordion animation for texts in detail page that's related to explore slide.

--- src/components/Overwatch2SlideExploreDetails.tsx
+++ src/components/Overwatch2SlideExploreDetails.tsx
@@ -6,9 +6,46 @@ import Overwatch2SlideExplore from "./Overwatch2SlideExplore";
 import anime from "animejs/lib/anime.es.js";
 import Overwatch2Carousel from "./Overwatch2Carousel";
 import { LG_SCREEN_SIZE, MED_SCREEN_SIZE } from "../constants";
+import useMeasure from "../useMeasure";
+import Overwatch2Accordion from "./Overwatch2Accordion";
 const timer = 3000;
 
 const Overwatch2: React.FC<{}> = () => {
+    const [selectedContentIndex, setSelectedContentIndex] = useState(0);
+    //@ts-ignore
+    const [bind, { height }] = useMeasure();
+    console.log(height, selectedContentIndex);
+    const showDesc = useTransition(selectedContentIndex, {
+        from: {
+            height: 0,
+            overflow: "hidden",
+        },
+        enter: {
+            height: height,
+        },
+
+        leave: {
+            height: 0,
+        },
+
+        config: {
+            duration: timer,
+        },
+    });
+
+    // const showDescSpring = useSpring({
+    //     from: {
+    //         overflow: "hidden",
+    //         height: 0,
+    //     },
+    //     to: {
+    //         height: selectedContentIndex === 0 ? height : 0,
+    //     },
+    //     config: {
+    //         duration: timer,
+    //     },
+    // });
+
     const { width } = useWindowDimensions();
     return (
         <React.Fragment>
@@ -25,24 +62,37 @@ const Overwatch2: React.FC<{}> = () => {
                 />
 
                 <div className="overwatch2SlideExploreDetailsTextContainer">
-                    {width > LG_SCREEN_SIZE && (
+                    {width >= LG_SCREEN_SIZE && (
                         <h1 className="overwatch2SlideExploreDetailsHeader">
                             Power Up And Save The World
                         </h1>
                     )}
-                    <div className="overwatch2SlideExploreDetailsTitleWrap">
+                    <div
+                        className="overwatch2SlideExploreDetailsTitleWrap"
+                        onClick={() => setSelectedContentIndex(0)}
+                    >
                         <div className="overwatch2SlideExploreDetailsTitleSelected"></div>
                         <h3 className="overwatch2SlideExploreDetailsTitle">
                             Action-packed story missions
                         </h3>
                     </div>
-                    <p className="overwatch2SlideExploreDetailsText">
-                        Play an active role in the next chapter of the Overwatch
-                        saga through a series of intense four-player missions.
-                        Fight back against Null Sector, uncover the motives
-                        behind the omnic attacks, and confront a rising wave of
-                        new threats.
-                    </p>
+
+                    <Overwatch2Accordion
+                        toggle={selectedContentIndex === 0 ? true : false}
+                    />
+
+                    <div
+                        onClick={() => setSelectedContentIndex(1)}
+                        className="overwatch2SlideExploreDetailsTitleWrap"
+                    >
+                        <div className="overwatch2SlideExploreDetailsTitleSelected"></div>
+                        <h3 className="overwatch2SlideExploreDetailsTitle">
+                            Action-packed story missions 2
+                        </h3>
+                    </div>
+                    <Overwatch2Accordion
+                        toggle={selectedContentIndex === 1 ? true : false}
+                    />
                 </div>
             </div>
         </React.Fragment>
